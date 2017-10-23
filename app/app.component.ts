f6b16f3e4cbf7b1b5e2dfed3eb13030d1bb30a75
Rebecca McIntosh and Sara Spink <sara@>
2017-10-23 11:29:31 -0700

    complete the basic setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+       <li *ngFor="let currentTask of tasks">{{currentTask.description}} <span id="priorityColor">{{currentTask.priority}}</span></li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 'high'),
+    new Task('Begin brainstorming possible JavaScript group projects', 'low'),
+    new Task('Add README file to last few Angular repos on GitHub', 'medium')
+  ];
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public priority: string) { }
 }
